fa77e4b9fd53a24f28c9fc7a604bdd804739ca56
paulofsj <paulo2012@gmail.com>
2018-09-15 18:53:42 -0300

    Update qr-scanner.js

--- qr-scanner.js
+++ qr-scanner.js
@@ -1,84 +1,109 @@
-if (require){
-  if (!angular) var angular = require('angular');
-  if (!qrcode) var qrcode = require('jsqrcode');
-}
+(function () {
+  'use strict';
 
-(function() {
-'use strict';
+  angular.module('qrScanner', ["ng"]).directive('qrScanner', ['$interval', '$window', function ($interval, $window) {
+    return {
+      restrict: 'E',
+      scope: {
+        ngSuccess: '&ngSuccess',
+        ngError: '&ngError',
+        ngVideoError: '&ngVideoError'
+      },
+      link: function (scope, element, attrs) {
 
-angular.module('qrScanner', ["ng"]).directive('qrScanner', ['$interval', '$window', function($interval, $window) {
-  return {
-    restrict: 'E',
-    scope: {
-      ngSuccess: '&ngSuccess',
-      ngError: '&ngError',
-      ngVideoError: '&ngVideoError'
-    },
-    link: function(scope, element, attrs) {
-    
-      window.URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
-      navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-    
-      var height = attrs.height || 300;
-      var width = attrs.width || 250;
-    
-      var video = $window.document.createElement('video');
-      video.setAttribute('width', width);
-      video.setAttribute('height', height);
-      video.setAttribute('style', '-moz-transform:rotateY(-180deg);-webkit-transform:rotateY(-180deg);transform:rotateY(-180deg);');
-      var canvas = $window.document.createElement('canvas');
-      canvas.setAttribute('id', 'qr-canvas');
-      canvas.setAttribute('width', width);
-      canvas.setAttribute('height', height);
-      canvas.setAttribute('style', 'display:none;'); 
-    
-      angular.element(element).append(video);
-      angular.element(element).append(canvas);
-      var context = canvas.getContext('2d'); 
-      var stopScan;
-    
-      var scan = function() {
-        if ($window.localMediaStream) {
-          context.drawImage(video, 0, 0, 307,250);
-          try {
-            qrcode.decode();
-          } catch(e) {
-            scope.ngError({error: e});
+        window.URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
+        navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+        var height = attrs.height || 300;
+        var width = attrs.width || 250;
+
+        var video = $window.document.createElement('video');
+        video.setAttribute('width', width);
+        video.setAttribute('height', height);
+        video.setAttribute('style', '-moz-transform:rotateY(-180deg);-webkit-transform:rotateY(-180deg);transform:rotateY(-180deg);');
+        var canvas = $window.document.createElement('canvas');
+        canvas.setAttribute('id', 'qr-canvas');
+        canvas.setAttribute('width', width);
+        canvas.setAttribute('height', height);
+        canvas.setAttribute('style', 'display:none;');
+
+        angular.element(element).append(video);
+        angular.element(element).append(canvas);
+        var context = canvas.getContext('2d');
+        var stopScan;
+
+        var scan = function () {
+          if ($window.localMediaStream) {
+            context.drawImage(video, 0, 0, 307, 250);
+            try {
+              qrcode.decode();
+            } catch (e) {
+              scope.ngError({ error: e });
+            }
           }
         }
-      }
 
-      var successCallback = function(stream) {
-        video.src = (window.URL && window.URL.createObjectURL(stream)) || stream;
-        $window.localMediaStream = stream;
+        var successCallback = function (stream) {
+          video.src = (window.URL && window.URL.createObjectURL(stream)) || stream;
+          $window.localMediaStream = stream;
 
-        scope.video = video;
-        video.play();
-        stopScan = $interval(scan, 500);
-      }
+          scope.video = video;
+          video.play();
+          stopScan = $interval(scan, 500);
+        }
 
-      // Call the getUserMedia method with our callback functions
-      if (navigator.getUserMedia) {
-        navigator.getUserMedia({video: true}, successCallback, function(e) {
-          scope.ngVideoError({error: e});
+        // Call the getUserMedia method with our callback functions
+        if (navigator.getUserMedia) {
+          navigator.getUserMedia({ video: true }, successCallback, function (e) {
+            scope.ngVideoError({ error: e });
+          });
+        } else {
+          scope.ngVideoError({ error: 'Native web camera streaming (getUserMedia) not supported in this browser.' });
+        }
+
+        qrcode.callback = function (data) {
+          scope.ngSuccess({ data: data });
+        };
+
+        element.bind('$destroy', function () {
+          stopMediaStream($window.localMediaStream);
+          if (stopScan) {
+            $interval.cancel(stopScan);
+          }
+
         });
-      } else {
-        scope.ngVideoError({error: 'Native web camera streaming (getUserMedia) not supported in this browser.'});
+
+        var stopMediaStream = function (mediaStream) {
+          if (!mediaStream) throw 'MediaStream argument is mandatory.';
+
+          // Latest firefox does support mediaStream.getAudioTrack but doesn't support stop on MediaStreamTrack
+          var checkForMediaStreamTrackStop = Boolean(
+            (mediaStream.getAudioTracks || mediaStream.getVideoTracks) && (
+              (mediaStream.getAudioTracks()[0] && !mediaStream.getAudioTracks()[0].stop) ||
+              (mediaStream.getVideoTracks()[0] && !mediaStream.getVideoTracks()[0].stop)
+            )
+          );
+
+          if (!mediaStream.getAudioTracks || checkForMediaStreamTrackStop) {
+            if (mediaStream.stop) {
+              mediaStream.stop();
+            }
+            return;
+          }
+
+          if (mediaStream.getAudioTracks().length && mediaStream.getAudioTracks()[0].stop) {
+            mediaStream.getAudioTracks().forEach(function (track) {
+              track.stop();
+            });
+          }
+
+          if (mediaStream.getVideoTracks().length && mediaStream.getVideoTracks()[0].stop) {
+            mediaStream.getVideoTracks().forEach(function (track) {
+              track.stop();
+            });
+          }
+        };
       }
-
-      qrcode.callback = function(data) {
-        scope.ngSuccess({data: data});
-      };
-
-      element.bind('$destroy', function() {
-        if ($window.localMediaStream) {
-          $window.localMediaStream.stop();
-        }
-        if (stopScan) {
-          $interval.cancel(stopScan);
-        }
-      });
     }
-  }
-}]);
+  }]);
 })();
